d2f936cedc9efab4510f0a73d907c02c6308a081
ljulie <lee95km@gmail.com>
2017-05-08 00:44:16 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,10 +5,49 @@
 // Date:
 // Title:
 ////////////////////////////////////////////////////////////////
-#include "stdafx.h"
+#include "Detect.h"
+
+CDetect start;
 
 int main()
 {
+	menu();
 
+	while (1);
 	return 0;
 }
+
+void menu() 
+{
+	char method;
+
+	cout << "Please Choose the method of driving: \n"
+		<< "Auto   (A) \n"
+		<< "Manual (M) \n"
+		<< endl;
+
+	cin >> method;
+
+	switch (method)
+	{
+	case 'A':
+	case 'a':
+		start.run();
+		break;
+
+	case 'M':
+	case 'm':
+		cout << "FWD (w) \n"
+			<< "LEFT (a) \n"
+			<< "RIGHT (d) \n"
+			<< "REVERSE (s) \n"
+			<< "Break (q) \n"
+			<< "Servo (e) \n"
+			<< "Stop (z)"
+			<< endl;
+
+		start.control();
+		break;
+	}
+}
+
